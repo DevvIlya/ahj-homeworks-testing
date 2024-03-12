51c4c981dab2628cebcdd7752930e9018a24a5a4
Ilya <yi1lei6a3@gmail.com>
2024-03-12 22:53:39 +0300

    Update validators.js

--- src/js/validators.js
+++ src/js/validators.js
@@ -1,25 +1,25 @@
 export function isValidInn(value) {
-    return value.length >= 10 && value.length <= 12;
+  return value.length >= 10 && value.length <= 12;
+}
+
+export function isValidCard(cardNumber) {
+  const number = cardNumber.trim();
+  if (!number) {
+    return false;
   }
-  
-  export function isValidCard(cardNumber) {
-    const number = cardNumber.trim();
-    if (!number) {
-      return false;
-    }
-  
-    const digits = cardNumber.trim().split('').map(Number);
-    let sum = 0;
-    for (let i = 0; i < digits.length; i += 1) {
-      let digit = digits[i];
-      if (i % 2 === 0) {
-        digit *= 2;
-        if (digit > 9) {
-          digit -= 9;
-        }
+
+  const digits = cardNumber.trim().split('').map(Number);
+  let sum = 0;
+  for (let i = 0; i < digits.length; i += 1) {
+    let digit = digits[i];
+    if (i % 2 === 0) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
       }
-      sum += digit;
     }
-    return sum % 10 === 0;
+    sum += digit;
   }
+  return sum % 10 === 0;
+}
   
